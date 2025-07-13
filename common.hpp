8e6d39514452f4a302c5d8d83ede494556972a2d
Kimathi Sedegah <sedkim370@gmail.com>
2025-07-13 10:01:32 +0000

    Create common.hpp

---
+++ common.hpp
@@ -0,0 +1,16 @@
+// common.hpp
+#pragma once
+#include <iostream>
+#include <thread>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <mutex>
+#include <map>
+#include <algorithm>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 9002
+#define BUFFER_SIZE 1024
